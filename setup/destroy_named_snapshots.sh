609379893fffc1ed057c9c13ec3c72587dabbc0e
Sven Göthel <sgothel@jausoft.com>
2026-06-19 13:33:13 +0200

    setup/destroy_named_snapshots.sh: Add using poolname for safety

--- setup/destroy_named_snapshots.sh
+++ setup/destroy_named_snapshots.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+poolname=$1
+shift
 snapname=$1
 shift
 
-if [ -z "$snapname" ] ; then
-    echo "Usage: $0 <snapname_to_be_destroyed>"
+if [ -z "$poolname" -o -z "$snapname" ] ; then
+    echo "Usage: $0 <pool-name> <snapname_to_be_destroyed>"
+    exit 0
 fi
 
-zfs list -t snapshot -H -o name | grep "@${snapname}" | xargs -n1 zfs destroy
+zfs list -t snapshot -H -o name -r ${poolname} | grep "@${snapname}" | xargs -n1 zfs destroy
