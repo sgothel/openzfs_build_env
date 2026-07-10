9de799241777a35974fe31e166d15067b0670c61
Sven Göthel <sgothel@jausoft.com>
2026-07-10 02:00:56 +0200

    zfs swap: comment-out disabling checksum

--- setup/new_full_zpool.sh
+++ setup/new_full_zpool.sh
@@ -95,7 +95,7 @@ if [ ! -z "$MYSWAPSIZE" ]; then
           -o primarycache=metadata -o secondarycache=none \
           -o com.sun:auto-snapshot=false $POOL/swap
 
-    zfs set checksum=off $POOL/swap
+    # zfs set checksum=off $POOL/swap
 
     mkswap -f /dev/zvol/$POOL/swap
     echo /dev/zvol/$POOL/swap none swap defaults 0 0 >> /etc/fstab
